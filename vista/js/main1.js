ded1b40a566eee688cc9e0ed286b99a748cf3878
JuanseCho <juansechoconta@gmail.com>
2023-09-01 19:12:39 -0500

    primer commit

---
+++ vista/js/main1.js
@@ -0,0 +1,51 @@
+$(function () {
+
+  $("#tabla_productos").on("click", "#btn_agregar", function () {
+
+    let codigo = $(this).attr("codigo")
+    let descripcion = $(this).attr("descripcion");
+    let valor = $(this).attr("valor");
+    
+      let itemFactura = '<tr class="se_itemsfactura">';
+      itemFactura += '<td   scope="row"><button type="button" id="btn_eliminar" class="btn btn-primary">remover</button></td>';
+      itemFactura += '<td id="codigo">' + codigo + '</td>';
+      itemFactura += '<td>' + descripcion + '</td>';
+      itemFactura += '<td><input name="" id="txt_cantidad" class="form-control" type="number" value="1" valorunitario="' + valor + '"></td>';
+      itemFactura += '<td id="td_valor">' + valor + '</td>';
+      itemFactura += '</tr>';
+
+    $("#contenedorFactura").append(itemFactura);
+    calculartotal();
+
+  })
+  $("#tablaFactura").on("click", "#btn_eliminar", function () {
+    $(this).parent().parent().remove();
+    calculartotal();
+
+
+  })
+
+  $("#tablaFactura").on("change", "#txt_cantidad", function () {
+
+    let cantidad = $(this).val();
+    let valorunitario = parseInt($(this).attr("valorunitario"));
+    let totalArticulo = cantidad * valorunitario;
+    $(this).parent().parent().children("#td_valor").html(totalArticulo);
+
+    calculartotal();
+
+  })
+
+  function calculartotal() {
+    let total = 0;
+
+    $(".se_itemsfactura").each(function () {
+      total += parseInt($(this).children("#td_valor").text());
+
+    })
+
+    $("#contenedortotal").html("<h1>total:" + total + "</h1>");
+
+  }
+
+})
